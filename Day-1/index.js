82fb5471e16ffdb730d12bfcda050000bf369735
Swaraj Jadhav <70018714+swaraj54@users.noreply.github.com>
2025-06-21 14:51:18 +0530

    Updates for day 1

---
+++ Day-1/index.js
@@ -0,0 +1,298 @@
+Data -
+
+akwbdgwahjk awdawdaw hjbgh32435@#$% 876543782 21 true/false
+Student Data - name, surname, email, number, age -> information, values
+
+JS - information type - > Data type
+
+Datatypes -
+String - "kjn$%^2345" '78654' `khbauwd` hgayuhwdn
+Number - 5632
+Boolean - true false
+
+
+
+
+
+
+
+
+console.log(1, "Hello", "1"); // print and calculations
+
+function - set of code -> help
+
+
+
+
+
+
+
+
+
+String -
+console.log("Hello", typeof "Hello");
+
+console.log("1", typeof "1");
+console.log(`true`, typeof "true");
+console.log("jhgh#$%^q3e4", typeof "jhgh#$%^q3e4");
+
+
+
+
+
+
+
+Number;
+
+console.log(1, typeof 1);
+
+
+
+
+
+
+
+
+
+Boolean
+
+console.log(true, typeof true);
+
+
+
+
+
+
+
+
+
+
+
+
+varibles - usecase - to store data
+
+var
+let
+const
+
+var
+
+synatx
+
+var varibleName = 23
+
+var userAge = 25;
+var userName = "Awdiz";
+
+declare = assign
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var userAge = 25;
+
+var userName = "Awdiz";
+
+var isUserCheck = false;
+
+console.log(userAge, userName, isUserCheck);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var counter = 1; // declare assign
+console.log(counter);
+counter = 2; // re-assign
+console.log(counter);
+var counter = 3; // re-declare
+console.log(counter);
+
+
+
+
+
+
+
+
+
+
+
+var counter1 = 10;
+var counter2 = 20;
+var result = counter1 + counter2
+console.log(result);
+
+
+
+
+
+
+
+
+
+
+var counter = 1; // declare assign
+
+counter = counter + 1;
+
+counter++; // increment opertor
+counter++; // increment opertor
+counter++; // increment opertor
+counter++; // increment opertor
+
+counter += 1
+console.log(counter);
+
+
+
+
+
+
+
+
+
+
+var counter = 1; // declare assign
+counter = counter - 1;
+counter--;  // decrement operator
+counter -= 10;
+console.log(counter);
+
+
+
+
+
+
+
+
+
+
+increment decrement
+assign re assign declare redeclare
+
+var counter;
+console.log(counter, typeof counter);
+
+counter = 1;
+console.log(counter);
+
+var counter;
+Hoisting
+
+let age = 10;
+console.log(age);
+
+age = 11;
+console.log(age);
+
+let age = 14;
+console.log(age)
+
+const age = 10;
+const age = 11
+console.log(age)
+
+age = 10;
+console.log(age)
+
+const age;
+
+
+
+
+
+
+
+
+
+
+
+
+            var         let         const
+reassign    true        true        false
+redelcare   true        false       false
+hosting     true        false       false
+scope       global      block       block
+
+
+
+
+
+block - {}
+
+
+
+
+
+{
+  const age = 20 ;
+  {
+    {
+      console.log(age);
+    }
+    console.log(age);
+  }
+  console.log(age);
+}
+
+// let userData = null;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+cd tab 
+cd ..
+mkdir folder/file 
+ls 
+arrow up button
+arrow down button 
+
+Datatypes -
+String
+Number 
+Boolean 
+undefined
+null 
+
+varibles - 
+var
+let 
+const 
+difference 
+hoisting 
+scope
+
+operators -
+increment
+decrment 
+
