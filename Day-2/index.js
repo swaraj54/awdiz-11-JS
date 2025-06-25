68778a345204be8fc42fe44ed5c2114a1f35f577
Swaraj Jadhav <70018714+swaraj54@users.noreply.github.com>
2025-06-25 21:08:21 +0530

    Updates for day 2

---
+++ Day-2/index.js
@@ -0,0 +1,103 @@
+// Datatypes -
+// String
+// Number
+// Boolean
+// undefined
+// null
+
+// varibles -
+// var
+// let
+// const
+// difference
+// hoisting
+// scope
+
+// operators -
+// increment
+// decrment
+// greter than
+// lesser than
+// equal too
+
+// Operator
+// Conditional Statement
+
+=  usecase assign
+==  usecase compare - Data
+=== usecase compare strict checking
+    data , datatypes
+
+
+
+
+
+
+
+
+console.log(1 == 1);
+console.log(1 == 2);
+console.log(10 == 2);
+console.log("10" == 2);
+console.log("2" == 2);
+check data not datatype
+console.log("10" === 2);
+console.log("2" === 2);
+check data and datatype
+
+
+
+
+
+
+console.log(2 > 1);
+console.log(2 > 10);
+console.log(2 < 1);
+console.log(2 < 10);
+console.log(2 <= 2);
+console.log(20 >= 20);
+2 < 2 false
+2 == 2   true
+2 <= 2
+
+
+
+
+
+
+
+
+
+
+And operator
+OR operator
+
+
+
+
+
+
+
+
+console.log(2 > 1 && 3 > 2 && 4 > 3 && 5 > 4);
+
+if all conditions are true then only it'll return true
+if any one of them or more than one is false then it'll return false
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log(2 > 10 || 3 > 20);
+
+
+if any of condition or more than one condition is true then it'll return true
+if all of conditions are false then only it'll return false
